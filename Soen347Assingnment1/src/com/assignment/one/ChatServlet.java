6e646289beed86088cd1d4f51ceb722cb3f97af0
abir_ <abir_mtl@hotmail.com>
2020-10-10 19:57:41 -0400

    Added controller, business layer. Its working

--- Soen347Assingnment1/src/com/assignment/one/ChatServlet.java
+++ Soen347Assingnment1/src/com/assignment/one/ChatServlet.java
@@ -1,7 +1,9 @@
 package com.assignment.one;
 
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/ChatServlet")
 public class ChatServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,19 +30,36 @@ public class ChatServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		String msg = request.getParameter("msg");
-		out.println(msg);
-		 
+		
+		String chats="";
+		for(Chat chat:ChatRepository.instance().getAllMessges())
+			chats+=chat.user+":"+chat.message+"\n";
+			
+		request.setAttribute("chats",chats );
+		
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String msg = request.getParameter("msg");
+		String user = request.getParameter("user");
+		
+		ChatRepository.instance().save(user, msg);
+		
+		doGet(request,response);
+		
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		ChatRepository.instance().deleteAll();
+		doGet(req, resp);
 	}
 
 }
